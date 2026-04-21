0e15a8581dacb0460a16d7cc1cbdb1f740cf6073
Minh Nguyen <minh@coastfirefinance.com>
2026-04-21 15:44:50 -0500

    Initial commit: CoastFIRE Finance PWA with SEO schema, bot-probe hardening, Search Console verification

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'coastfire-v21';
+const LOCAL = [
+  './', './index.html', './manifest.json', './icon.svg',
+  './blog/what-is-coastfire.html',
+  './blog/coastfire-vs-baristafire.html',
+  './blog/coastfire-at-30-vs-40.html',
+  './blog/my-coastfire-journey.html',
+  './blog/5-coastfire-mistakes.html',
+  './blog/4-percent-rule-coastfire.html'
+];
+const CDN   = 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(LOCAL).then(() => c.add(CDN).catch(() => {}))
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
